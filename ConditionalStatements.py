2e6c4530c672419e38d34c743c0fbabf3759efbd
vishal arora <35168406+HASHtechvishal@users.noreply.github.com>
2023-07-14 00:25:12 +0530

    Add files via upload

---
+++ ConditionalStatements.py
@@ -0,0 +1,14 @@
+#interview qus conditional statements
+
+'''
+1. What are the three main conditional statements in Python?
+2. What constitutes “True” in Python?
+3. What are the comparison operators in Python?
+4. Illustrate a basic if, elif, else structure.
+5. name the keyword witch help in weiting code involves condition
+6. write the syntax of simple if statement
+7. is there any limit of statement that can appear under an if block
+8. write a program to check whether a person is eligible for voting or not
+9. write a program to check whether a mumber entered by user is even or odd
+10. write a program to check whether an years is leap year or not
+'''
