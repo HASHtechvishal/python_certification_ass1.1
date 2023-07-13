0999ad2f7dd89fe2b70e5f089bd1e6c5a2ec2809
vishal arora <35168406+HASHtechvishal@users.noreply.github.com>
2023-07-14 00:11:02 +0530

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,13 @@
+#Operators interview questions
+'''
+1. What is an Operator in general?
+2. What is meant by an Operator in Python?
+3. Types of operators in Python
+4. What is the another name of Comparison Operators?
+5. Which Operator is used for assigning a value into a variable?
+6. Which Operator is used for comparison of values?
+7. What is the use of “+” operator?
+8. Write syntax for “greater than or equal to” operator.
+9. How many types of operations are supported by the Python language?
+10. What do you mean by Operator precedence?
+'''
