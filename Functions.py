bf0adb6490e816674cfe4ff523f030f8f0d79c07
vishal arora <35168406+HASHtechvishal@users.noreply.github.com>
2023-07-14 00:40:00 +0530

    Add files via upload

---
+++ Functions.py
@@ -0,0 +1,13 @@
+# interview qus on Functions
+'''
+1. What are the two types of functions in Python?
+2. Can you explain what a call graph is? How do you create one?
+3. When should you use anonymous functions and when should you use regular ones?
+4. Do Python functions have return values? If yes, then how many can they have?
+5. Why does Python support both positional and keyword arguments to its functions?
+6. Is it possible for a function’s code to read from or write to variables defined outside that function? If yes, then how?
+7. Can you explain what a closure is in Python? How do you implement closures?
+8. Can you explain what decorators are in Python?
+9. Is there any way to define static methods in Python? If yes, then how?
+10. Is it possible to pass a variable number of arguments to a function in Python? If yes, then how?
+'''
