cc5b55107b9e8fed21bf6a321ab0df7fc49906a6
vishal arora <35168406+HASHtechvishal@users.noreply.github.com>
2023-07-14 00:33:51 +0530

    Add files via upload

---
+++ LoopingStatements.py
@@ -0,0 +1,12 @@
+# interview qus on Looping Statements
+'''
+1. What is the difference between a for loop and a while loop?
+2. What is the Python syntax for a for loop?
+3. What are the advantages of using a for loop in Python?
+4. What are the disadvantages of using a for loop in Python?
+5. How can I use a break statement in my Python for loops?
+6. what is the difference between the continue and break statements in Python?
+7. What is the purpose of a pass command in python?
+8. How does For loop and While loop differ in Python and when do you choose to use them?
+
+'''
