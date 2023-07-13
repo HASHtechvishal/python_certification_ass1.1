cbdffce99bb360fc5ebe7573ad6b6073848be55c
vishal arora <35168406+HASHtechvishal@users.noreply.github.com>
2023-07-13 23:45:13 +0530

    Add files via upload

---
+++ data_type20qus.py
@@ -0,0 +1,12 @@
+'''Data type 20 interview questions '''
+'''
+1. What are Data Types?
+2. Name four of the main data types in Python?
+3. Why are these data types known as Python’s core data types?
+4. What does immutable mean and what three types of Python core data types are considered immutable?
+5. What does sequence mean and which three types of data fall into this category?
+6. What does mapping mean and what kind of data type is based on mapping?
+7. What is polymorphism and why should you care?
+8. What are the common built-in data types in Python?
+
+'''
